d0d77ee56c009074083438b0078609b7d3f87056
Michael Diaz <mike.diaz006@gmail.com>
2019-12-19 21:02:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,6 +9,8 @@ def map(array)
 end
 
 def reduce(array, starting_point = 0)
+pp array
+pp starting_point
 count = 0
   while count < array.length 
     starting_point = yield(starting_point, array[count])
