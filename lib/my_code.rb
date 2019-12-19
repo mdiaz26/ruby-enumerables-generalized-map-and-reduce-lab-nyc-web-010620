0a5ba24405d50317ea3c593e6095032afb3f2c9e
Michael Diaz <mike.diaz006@gmail.com>
2019-12-19 20:56:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,9 +8,10 @@ def map(array)
   new_array
 end
 
-def reduce(array, starting_point = nil)
-  array.reduce do |element, n|
-    n += yield(element, n)
-    # binding.pry
+def reduce(array, starting_point = 0)
+count = 0
+  while count < array.length do |element|
+  count += yield(element)
+    end
   end
 end
