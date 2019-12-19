799eaead582444a1b0ce97f68490891aa9a86a06
Michael Diaz <mike.diaz006@gmail.com>
2019-12-19 20:46:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
-# Your Code Here
+require "pry"
+
+def map(array)
+  new_array = []
+  array.map do |element|
+    new_array << yield(element)
+  end
+  new_array
+end
+
+def reduce(array)
+  array.reduce do |element|
+    n += yield(element, n = 0)
+    binding.pry
+  end
+end
