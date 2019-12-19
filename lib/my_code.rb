bd9a47c867817c97f04baeed3dff118d86bbc680
Michael Diaz <mike.diaz006@gmail.com>
2019-12-19 21:16:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+require "pry"
+
+def map(array)
+  new_array = []
+  array.map do |element|
+    new_array << yield(element)
+  end
+  new_array
+end
+
+def reduce(array, starting_point = nil)
+  if starting_point
+    result = starting_point
+    count = 0
+  else
+    result = array[0]
+    count = 1
+  end
+  while count < array.length 
+    result = yield(result, array[count])
+  count += 1 
+  end
+  result
+end
